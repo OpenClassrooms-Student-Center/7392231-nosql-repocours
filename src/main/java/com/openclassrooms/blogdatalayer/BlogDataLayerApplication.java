3eed657c52286e69da3ae439de0ee4e132f3d0da
romainsessa <romainsessa@gmail.com>
2021-11-15 19:59:49 +0100

    create minimal structure

---
+++ src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.blogdatalayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDataLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDataLayerApplication.class, args);
+	}
+
+}
