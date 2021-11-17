eebb781c868af0bd9f1b13d3f7ba46fb5466c43d
romainsessa <romainsessa@gmail.com>
2021-11-17 20:20:54 +0100

    Use CommandLineRunner interface on BlogDataLayerApplication class

--- src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
+++ src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
@@ -1,13 +1,29 @@
 package com.openclassrooms.blogdatalayer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.openclassrooms.blogdatalayer.repository.PostRepository;
+
 @SpringBootApplication
-public class BlogDataLayerApplication {
+public class BlogDataLayerApplication implements CommandLineRunner {
+
+	private final Logger logger = LoggerFactory.getLogger(BlogDataLayerApplication.class);
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogDataLayerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Start !");
 	}
 
 }
