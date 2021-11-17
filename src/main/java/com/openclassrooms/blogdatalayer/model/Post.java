f6691a55ca8e7e9b3f7aab5b59ac9091a22368b7
romainsessa <romainsessa@gmail.com>
2021-11-17 20:45:46 +0100

    Define index on name property of Post class

--- src/main/java/com/openclassrooms/blogdatalayer/model/Post.java
+++ src/main/java/com/openclassrooms/blogdatalayer/model/Post.java
@@ -3,6 +3,7 @@ package com.openclassrooms.blogdatalayer.model;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "posts")
@@ -10,6 +11,7 @@ public class Post {
  
 	@Id
 	private String id;
+	@Indexed(unique = true)
 	private String name;
 	private String content;
 	private Date date;
