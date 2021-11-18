0f3f6050238b3635cd770bec46e14304adbeb763
romainsessa <romainsessa@gmail.com>
2021-11-18 12:04:02 +0100

    Update the name of an existing tutorial

--- src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
+++ src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
@@ -31,44 +31,10 @@ public class BlogDataLayerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Add one tutorial
-		Tutorial newTutorial = new Tutorial();
-		newTutorial.setName("How to create a Spring Boot application");
-		newTutorial.setShortDescription("I will explain you how to use Spring Boot to build Java application.");
-		newTutorial.setContent("Here we are, Spring Boot is a fantastic module of Spring Framework. It allows auto configuration....");
-		newTutorial.setCategory("Spring Boot");
-		
-		tutorialRepository.insert(newTutorial);
-		
-		// Add few tutorials
-		Tutorial newTuto1 = new Tutorial();
-		newTuto1.setName("Deploy a Spring Boot app with Docker");
-		newTuto1.setShortDescription("...");
-		newTuto1.setContent("...");
-		newTuto1.setCategory("Spring Boot");
-		
-		Tutorial newTuto2 = new Tutorial();
-		newTuto2.setName("Build a microservice architecture with Spring Cloud");
-		newTuto2.setShortDescription("...");
-		newTuto2.setContent("...");
-		newTuto2.setCategory("Spring Cloud");
-		
-		tutorialRepository.insert(List.of(newTuto1, newTuto2));
-		
-		// Add a tutorial with a Tag
-		Tag springTag = new Tag();
-		springTag.setName("Spring Framework");
-		springTag.setSlug("spring-framework");
-		springTag.setDescription("Spring is the most Java framework used on the world.");
-		
-		Tutorial newTuto3 = new Tutorial();
-		newTuto3.setName("How Spring implements the dependency injection pattern");
-		newTuto3.setShortDescription("...");
-		newTuto3.setContent("...");
-		newTuto3.setCategory("Spring");
-		newTuto3.setTag(springTag);
-		
-		tutorialRepository.insert(newTuto3);
+		Tutorial existingTutorial = tutorialRepository.findById("6192c22d783f4a2a0a7d9bf3").get();
+		String newTutorialName = existingTutorial.getName() + " [modified]";
+		existingTutorial.setName(newTutorialName);
+		tutorialRepository.save(existingTutorial);
 	}
 
 }
