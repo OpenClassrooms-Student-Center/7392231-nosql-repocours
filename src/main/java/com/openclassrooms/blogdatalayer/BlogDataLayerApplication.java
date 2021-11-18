4217a610b0393b8f58bd346e4fc52281f080ecf6
romainsessa <romainsessa@gmail.com>
2021-11-18 11:52:23 +0100

    Add one tutorial and few tutorials

--- src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
+++ src/main/java/com/openclassrooms/blogdatalayer/BlogDataLayerApplication.java
@@ -1,11 +1,15 @@
 package com.openclassrooms.blogdatalayer;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.openclassrooms.blogdatalayer.model.Tutorial;
 import com.openclassrooms.blogdatalayer.repository.PostRepository;
 import com.openclassrooms.blogdatalayer.repository.TutorialRepository;
 
@@ -26,7 +30,29 @@ public class BlogDataLayerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-	
+		// Add one tutorial
+		Tutorial newTutorial = new Tutorial();
+		newTutorial.setName("How to create a Spring Boot application");
+		newTutorial.setShortDescription("I will explain you how to use Spring Boot to build Java application.");
+		newTutorial.setContent("Here we are, Spring Boot is a fantastic module of Spring Framework. It allows auto configuration....");
+		newTutorial.setCategory("Spring Boot");
+		
+		tutorialRepository.insert(newTutorial);
+		
+		// Add few tutorials
+		Tutorial newTuto1 = new Tutorial();
+		newTuto1.setName("Deploy a Spring Boot app with Docker");
+		newTuto1.setShortDescription("...");
+		newTuto1.setContent("...");
+		newTuto1.setCategory("Spring Boot");
+		
+		Tutorial newTuto2 = new Tutorial();
+		newTuto2.setName("Build a microservice architecture with Spring Cloud");
+		newTuto2.setShortDescription("...");
+		newTuto2.setContent("...");
+		newTuto2.setCategory("Spring Cloud");
+		
+		tutorialRepository.insert(List.of(newTuto1, newTuto2));
 	}
 
 }
